fd82cfd40e9091d3450d9d6bfc142dce7ccdf31f
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2016-04-26 17:39:24 -0500

    Refactor recaudadora and vehiculo
    Place constructors, getters and setters for properties

--- src/dto/VehiculoDTO.java
+++ src/dto/VehiculoDTO.java
@@ -23,4 +23,216 @@ public class VehiculoDTO {
 	public String nrpv;
 	public String uso;
 	public String pasaj;
+
+	public VehiculoDTO() {
+
+	}
+
+	public VehiculoDTO(String id) {
+
+	}
+
+	public VehiculoDTO(String placas, String color1, String mov, String clase, String marca, String color2,
+			String proced, String tipo, String cc, String submarca, String numeroSerie, String comb, String cilindros,
+			String version, String numeroMotor, String servicio, String puertas, String modelo, String nrpv, String uso,
+			String pasaj) {
+		this.placas = placas;
+		this.color1 = color1;
+		this.mov = mov;
+		this.clase = clase;
+		this.marca = marca;
+		this.color2 = color2;
+		this.proced = proced;
+		this.tipo = tipo;
+		this.cc = cc;
+		this.submarca = submarca;
+		this.numeroSerie = numeroSerie;
+		this.comb = comb;
+		this.cilindros = cilindros;
+		this.version = version;
+		this.numeroMotor = numeroMotor;
+		this.servicio = servicio;
+		this.puertas = puertas;
+		this.modelo = modelo;
+		this.nrpv = nrpv;
+		this.uso = uso;
+		this.pasaj = pasaj;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPlacas() {
+		return placas;
+	}
+
+	public void setPlacas(String placas) {
+		this.placas = placas;
+	}
+
+	public String getColor1() {
+		return color1;
+	}
+
+	public void setColor1(String color1) {
+		this.color1 = color1;
+	}
+
+	public String getMov() {
+		return mov;
+	}
+
+	public void setMov(String mov) {
+		this.mov = mov;
+	}
+
+	public String getClase() {
+		return clase;
+	}
+
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getColor2() {
+		return color2;
+	}
+
+	public void setColor2(String color2) {
+		this.color2 = color2;
+	}
+
+	public String getProced() {
+		return proced;
+	}
+
+	public void setProced(String proced) {
+		this.proced = proced;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCc() {
+		return cc;
+	}
+
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+
+	public String getSubmarca() {
+		return submarca;
+	}
+
+	public void setSubmarca(String submarca) {
+		this.submarca = submarca;
+	}
+
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+
+	public String getComb() {
+		return comb;
+	}
+
+	public void setComb(String comb) {
+		this.comb = comb;
+	}
+
+	public String getCilindros() {
+		return cilindros;
+	}
+
+	public void setCilindros(String cilindros) {
+		this.cilindros = cilindros;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getNumeroMotor() {
+		return numeroMotor;
+	}
+
+	public void setNumeroMotor(String numeroMotor) {
+		this.numeroMotor = numeroMotor;
+	}
+
+	public String getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(String servicio) {
+		this.servicio = servicio;
+	}
+
+	public String getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(String puertas) {
+		this.puertas = puertas;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getNrpv() {
+		return nrpv;
+	}
+
+	public void setNrpv(String nrpv) {
+		this.nrpv = nrpv;
+	}
+
+	public String getUso() {
+		return uso;
+	}
+
+	public void setUso(String uso) {
+		this.uso = uso;
+	}
+
+	public String getPasaj() {
+		return pasaj;
+	}
+
+	public void setPasaj(String pasaj) {
+		this.pasaj = pasaj;
+	}
+
 }
