1b70cbb6e6f9da17fb3253e10d2e84f0dd89e446
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2016-04-26 17:23:26 -0500

    Implement Conexion class
    Class that handles DB connections

---
+++ src/connection/Conexion.java
@@ -0,0 +1,53 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	/* 
+	 * Pregunta si la instancia ya esta creada
+	 * para usar la misma y si no se crea
+	 * una nueva
+	 */
+	public static Conexion instance; // Singleton
+	private Connection cnn;
+	
+	/*
+	 * Constructor privado
+	 * para no crear instancias desde afuera
+	 */
+	private Conexion() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); // driver
+			cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/vialidad", "root", "asda");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Unica forma de crear una conexion y que aplica el Singleton.
+	 * La palabra synchronized hace una lista de espera para que
+	 * si hay muchos usuarios, estos esperen su turno hasta que termine
+	 * el usuario que lo está usando
+	 */
+	public synchronized static Conexion saberEstado() {
+		if (instance == null) {
+			instance = new Conexion();
+		}
+		return instance;
+	}
+	
+	public Connection getConnection() {
+		return cnn;
+	}
+	
+	public void cerrarConexion() {
+		instance = null;
+	}
+}
